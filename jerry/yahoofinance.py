a89487d0d42e5f9462fb370975cccbfa417257e8
01Jerry10 <rd324278@gmail.com>
2024-05-31 09:08:31 +0530

    Streamlit learning in python venv.

--- jerry/yahoofinance.py
+++ jerry/yahoofinance.py
@@ -22,9 +22,13 @@ st.write(
 )
 import datetime
 
-start_date = st.date_input("Enter start date", datetime.date(2019, 1, 1))
+col1, col2 = st.columns(2)
 
-end_date = st.date_input("Enter end date", datetime.date(2022, 12, 31))
+with col1:
+    start_date = st.date_input("Enter start date", datetime.date(2019, 1, 1))
+
+with col2:
+    end_date = st.date_input("Enter end date", datetime.date(2022, 12, 31))
 
 ticker_symbol = 'MSFT'
 st.write(ticker_symbol)
