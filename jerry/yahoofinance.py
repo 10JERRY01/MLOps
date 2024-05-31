c64834223475fdb1ebf8b836965c3bdc16d07023
01Jerry10 <rd324278@gmail.com>
2024-05-31 10:56:21 +0530

    Streamlit learning in python venv. Choose company and get stocks data.

--- jerry/yahoofinance.py
+++ jerry/yahoofinance.py
@@ -52,3 +52,42 @@ st.write(
 )
 
 st.line_chart(ticker_df.Volume)
+
+#Using yahoo finance to see the stock prices of any company using streamlit between fixed dates.
+
+st.write(
+    '''
+        # This app shows microsoft stocks and lets you choose the dates 
+    '''
+)
+import datetime
+
+col1, col2 = st.columns(2)
+
+with col1:
+    start_date = st.date_input("Enter start date", datetime.date(2018, 1, 1))
+
+with col2:
+    end_date = st.date_input("Enter end date", datetime.date(2021, 12, 31))
+
+ticker_symbol = st.text_input('Enter Stock Symbol', 'MSFT', key='placeholder')
+st.write(ticker_symbol)
+ticker_date = yf.Ticker(ticker_symbol)
+ticker_df = ticker_date.history(period='1d', start=f'{start_date}', end=f'{end_date}')
+
+st.dataframe(ticker_df)
+
+st.write(
+    """
+        ## Daily Closing Price Chart
+    """
+)
+
+st.line_chart(ticker_df.Close)
+
+st.write(
+    """
+        ## Volume Of Shares Traded Each Day
+    """
+)
+st.line_chart(ticker_df.Volume)
